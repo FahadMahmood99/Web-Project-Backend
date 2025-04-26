95ade549692431f647c5c8692fb950b8d9d7da99
ShahmirShehzad <shahmirshehzad@gmail.com>
2025-04-26 22:39:27 +0500

    added book model

---
+++ models/Book.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+      trim: true,
+    },
+    publication_year: {
+      type: Int,
+      required: [true, "Publication year is required"],
+      trim: true,
+    },
+    cover_image_url: {
+      type: String,
+      required: [true, "Cover image url is required"],
+      trim: true,
+    },
+    no_of_sections: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Automatically generate `id` (alternative to `_id`)
+BookSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+BookSchema.set("toJSON", { virtuals: true });
+
+module.exports = mongoose.model("Book", BookSchema);
